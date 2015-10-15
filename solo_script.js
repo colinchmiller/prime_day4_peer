d00ab790af57fc7771ab61072490bce1ed7faaa3
colinmiller <colinchmiller@gmail.com>
2015-10-15 17:46:00 -0500

    Clean up and comments added

--- solo_script.js
+++ solo_script.js
@@ -1,26 +1,16 @@
-// ! ! !
-// Three Bugs
-//bug #1: line 22: calculateSTI(array) requires the [i] index to iterate through the arrays
-//bug #2: line 69: basePercent should not be subtracted by 1
-//bug #3: line 44: when reassigning the value of newArray[2] to the total of the bonus plus the base salary, needed to parseInt the value to round the result and put parenthesis around the 1 + bonus to override the order of operations
-
+//the original employee objects
 var objectAtticus = {name :"Atticus", employeeNumber: "2405", baseSalary: "47000", reviewScore: 3};
 var objectJem = {name: "Jem", employeeNumber: "62347", baseSalary: "63500", reviewScore: 4};
 var objectBoo = {name: "Boo", employeeNumber: "11435", baseSalary: "54000", reviewScore: 3};
 var objectScout = {name: "Scout", employeeNumber: "6243", baseSalary: "74750", reviewScore: 5};
 
+//creating an array and placing the objects into it in order to iterate
 var array = [objectAtticus, objectJem, objectBoo, objectScout];
 
-//Create variables used to write to the DOM
-//var newEl, newText, position;
-//Capture the position of insertion into the DOM
-//position = document.getElementById('content');
 
-//Loop the array, extracting each array and writing information to the DOM
-//Note that the information is not 'clean'
 $(document).ready(function(){
+  //the button console print function
   $("#bigContainer").on('click', '.benjamin', function(){
-
     for(i = 0; i < array.length; i++){
       console.log(array[i].name);
       console.log(array[i].bonusRate);
@@ -29,12 +19,13 @@ $(document).ready(function(){
       }
   });
 
-
+//creates divs and places employee data within in html
 for(var i = 0; i < array.length; i++){
   $("#bigContainer").append("<div class='miniContainer'></div>");
   var $el = $("#bigContainer").children().last();
+  //calling the function to calculate the bonuses/////
   array[i] = calculateSTI(array[i]);
-  //console.log("this is array[i]: ", array[i]);
+  ///////////////////////////////////////////////////
   $el.append("<p>" + array[i].name + "</p>");
   $el.append("<p>" + array[i].bonusRate + "</p>");
   $el.append("<p>" + array[i].totalIncome + "</p>");
@@ -43,12 +34,8 @@ for(var i = 0; i < array.length; i++){
   };
 });
 
- // 	newEl = document.createElement('li');
-	// newText = document.createTextNode(array[i].employeeName + " " + newObject.employeeName + " " + newObject.bonusRate + " " + newObject.totalIncome + " " + newObject.bonusAmount);
- //  console.log("This is the DOM text: " , newText);
-	// newEl.appendChild(newText);
-	// position.appendChild(newEl);
 
+//the function to calculate the bonus information
 function calculateSTI(array){
   var newObjectFirst = {};
   var newObjectSecond = {};
@@ -59,15 +46,13 @@ function calculateSTI(array){
   newObjectFirst.baseSalary = array.baseSalary;
   newObjectFirst.reviewScore = array.reviewScore;
 
-  // var employeeNumber = array.employeeNumber;
-  // var baseSalary = array.baseSalary;
-  // var reviewScore = array.reviewScore;
-
+//the variable bonus
   var bonus = getBaseSTI(newObjectFirst.reviewScore) + getYearAdjustment(newObjectFirst.employeeNumber) - getIncomeAdjustment(newObjectFirst.baseSalary);
   if(bonus > 0.13){
     bonus = 0.13;
   }
 
+//calculating, console logging, and returning the employee bonus
   newObjectSecond.name = newObjectFirst.employeeName;
   newObjectSecond.bonusRate = bonus;
   newObjectSecond.totalIncome = Math.round(newObjectFirst.baseSalary * (1.0 + bonus)); //BUG #3 Need to parenthesize 1.0 + bonus to override order of operations and parseInt to round to whole number
@@ -76,6 +61,7 @@ function calculateSTI(array){
   return newObjectSecond;
 }
 
+//used to calculate variable bonus
 function getBaseSTI(reviewScore){
   var basePercent;
   switch(reviewScore){
@@ -98,6 +84,7 @@ function getBaseSTI(reviewScore){
   return basePercent; 
 }
 
+//used to calculate variable bonus
 function getYearAdjustment(employeeNumber){
   var yearAdjustment = 0;
   if(employeeNumber.length == 4){
@@ -106,6 +93,7 @@ function getYearAdjustment(employeeNumber){
   return yearAdjustment;
 }
 
+//used to calculate variable bonus
 function getIncomeAdjustment(salary){
   var incomeAdjustment = 0;
   salary = parseInt(salary);
